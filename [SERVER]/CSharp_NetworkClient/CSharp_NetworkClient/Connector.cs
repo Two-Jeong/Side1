2f4cfca8751b360b7e1dc7307dc82e2ad166c2c0
정은성 <dev.eunseong@gmail.com>
2025-12-22 20:12:51 +0900

    🐛 :: CSharpNetworkLibrary 버전 변경
    - dll Unity 호환을 위해 .Net Core 3.0버전으로 변경

--- [SERVER]/CSharp_NetworkClient/CSharp_NetworkClient/Connector.cs
+++ [SERVER]/CSharp_NetworkClient/CSharp_NetworkClient/Connector.cs
@@ -1,55 +1,64 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
 using System.Net.Sockets;
 
-namespace CSharp_NetworkClient;
-
-public class Connector
+namespace CSharp_NetworkClient
 {
-    private Func<Session> m_session_factory;
-    public void Connect(IPEndPoint end_point, int count, Func<Session> factory)
+
+    public class Connector
     {
-        m_session_factory = factory;
-        
-        for(int i = 0; i < count; ++i)
+        private Func<Session> m_session_factory;
+        public void Connect(IPEndPoint end_point, int count, Func<Session> factory)
         {
-            Socket socket = new Socket(end_point.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            m_session_factory = factory;
 
-            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
-            args.Completed += CompleteConnect;
-            args.RemoteEndPoint = end_point;
-            args.UserToken = socket;
-
-            if (false == StartConnect(args))
+            for (int i = 0; i < count; ++i)
             {
-                //TODO: LOG
-                return;
+                Socket socket = new Socket(end_point.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += CompleteConnect;
+                args.RemoteEndPoint = end_point;
+                args.UserToken = socket;
+
+                if (false == StartConnect(args))
+                {
+                    //TODO: LOG
+                    return;
+                }
             }
         }
-    }
 
 
-    private bool StartConnect(SocketAsyncEventArgs args)
-    {
-        Socket socket = args.UserToken as Socket;
-        
-        if (null == socket)
-            return false;
-        
-        bool is_pending = socket.ConnectAsync(args);
-        if(false == is_pending)
-            CompleteConnect(null, args);
-
-        return true;
-    }
-
-    private void CompleteConnect(object sender, SocketAsyncEventArgs args)
-    {
-        if (args.SocketError == SocketError.Success)
+        private bool StartConnect(SocketAsyncEventArgs args)
         {
-            Session session = m_session_factory.Invoke();
-            session.Init(args.ConnectSocket, args.RemoteEndPoint);
-            
-            session.OnConnected();
+            Socket socket = args.UserToken as Socket;
+
+            if (null == socket)
+                return false;
+
+            bool is_pending = socket.ConnectAsync(args);
+            if (false == is_pending)
+                CompleteConnect(null, args);
+
+            return true;
+        }
+
+        private void CompleteConnect(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                Session session = m_session_factory.Invoke();
+                session.Init(args.ConnectSocket, args.RemoteEndPoint);
+
+                session.OnConnected();
+            }
         }
     }
 }
--- [SERVER]/CSharp_NetworkClient/CSharp_NetworkClient/TestSession.cs
+++ [SERVER]/CSharp_NetworkClient/CSharp_NetworkClient/TestSession.cs
@@ -1,60 +1,70 @@
-﻿namespace CSharp_NetworkClient;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
-public class TestSession : Session
+namespace CSharp_NetworkClient
 {
-    protected override void InitPacketHandlers()
-    {
-        m_packet_handlers.Add(Convert.ToUInt16(packet_number.TestEcho), OnRecvTestEcho);
-    }
 
-    public override void OnConnected()
+    public class TestSession : Session
     {
-        Thread thread = new Thread(test_session_loop_func);
-        thread.Start();
-    }
-
-    public override void OnDisconnected()
-    {
-    }
-
-    public override void OnSend()
-    {
-    }
-
-    private void test_session_loop_func()
-    {
-        Random random = new Random();
-        while (true)
+        protected override void InitPacketHandlers()
         {
-            C2S_TestEcho send_packet = new C2S_TestEcho();
-            send_packet.RandNumber = random.Next(100);
-
-            DoSend(send_packet);
-            
-            Thread.Sleep(1000);
-        }
-    }
-    private bool OnRecvTestEcho(Packet packet)
-    {
-        S2C_TestEcho recv_pacekt_from_server = new S2C_TestEcho();
-        packet.PopData(recv_pacekt_from_server);
-
-        Console.WriteLine($"echo packet recived: session id: {recv_pacekt_from_server.SessionId}, random number: {recv_pacekt_from_server.RandNumber}");
-        return true;
-    }
-
-    protected override void OnPacketAssambled(Packet packet)
-    {
-        if (false == m_packet_handlers.ContainsKey(packet.Protocol))
-        {
-            //TODO: LOG
-            return;
+            m_packet_handlers.Add(Convert.ToUInt16(packet_number.TestEcho), OnRecvTestEcho);
         }
 
-        if (false == m_packet_handlers[packet.Protocol]?.Invoke(packet))
+        public override void OnConnected()
         {
-            //TODO: LOG AND Disconnect
-            return;
-        } 
+            Thread thread = new Thread(test_session_loop_func);
+            thread.Start();
+        }
+
+        public override void OnDisconnected()
+        {
+        }
+
+        public override void OnSend()
+        {
+        }
+
+        private void test_session_loop_func()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                C2S_TestEcho send_packet = new C2S_TestEcho();
+                send_packet.RandNumber = random.Next(100);
+
+                DoSend(send_packet);
+
+                Thread.Sleep(1000);
+            }
+        }
+        private bool OnRecvTestEcho(Packet packet)
+        {
+            S2C_TestEcho recv_pacekt_from_server = new S2C_TestEcho();
+            packet.PopData(recv_pacekt_from_server);
+
+            Console.WriteLine($"echo packet recived: session id: {recv_pacekt_from_server.SessionId}, random number: {recv_pacekt_from_server.RandNumber}");
+            return true;
+        }
+
+        protected override void OnPacketAssambled(Packet packet)
+        {
+            if (false == m_packet_handlers.ContainsKey(packet.Protocol))
+            {
+                //TODO: LOG
+                return;
+            }
+
+            if (false == m_packet_handlers[packet.Protocol]?.Invoke(packet))
+            {
+                //TODO: LOG AND Disconnect
+                return;
+            }
+        }
     }
 }
