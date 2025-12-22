c583788b859512f4e0e9961d840db2f675fa242a
burger <burger@111percent.net>
2025-12-22 19:39:48 +0900

    퀘스트 에러 코드 수정

--- [CLIENT]/Assets/02.Scripts/Quest/Reward/PointReward.cs
+++ [CLIENT]/Assets/02.Scripts/Quest/Reward/PointReward.cs
@@ -7,7 +7,7 @@ public class PointReward : Reward
 {
     public override void Give(Quest quest)
     {
-        GameSystem.Instance.AddScore(Quantity);
+        //GameSystem.Instance.AddScore(Quantity);
         PlayerPrefs.SetInt("bonusScore", Quantity);
         PlayerPrefs.Save();
     }
